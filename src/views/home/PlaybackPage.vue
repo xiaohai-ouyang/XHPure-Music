b19bb298f745b1731bfda5dd20ef89a2cee81b5b
xiaohai-ouyang <xouyang525@gmail.com>
2025-10-03 19:56:20 +0800

    add
    新增跳转到播放页

---
+++ src/views/home/PlaybackPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { usePageStatusStore } from '@/stores/pageStatusStores'
+</script>
+
+<template>
+  <div class="myPlaybackPage">
+    <button @click="usePageStatusStore().isPlayBackExpand = false">关闭</button>
+  </div>
+</template>
