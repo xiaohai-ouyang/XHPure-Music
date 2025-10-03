11074628f0f2738b3f93a7ea084a73cf3b402533
xiaohai-ouyang <xouyang525@gmail.com>
2025-10-03 20:22:15 +0800

    删除逻辑

--- src/main.ts
+++ src/main.ts
@@ -13,23 +13,7 @@ app.use(pinia)
 app.use(router)
 
 import { usePageStatusStore } from '@/stores/pageStatusStores'
-import { watch } from 'vue'
-
 const pageStore = usePageStatusStore()
-
-watch(
-  () => pageStore.isPlayBackExpand,
-  (newVal) => {
-    const currentPath = router.currentRoute.value.path
-
-    if (newVal && currentPath !== '/playback') {
-      router.push('/playback')
-    } else if (!newVal && currentPath === '/playback') {
-      router.back()
-    }
-  },
-  { immediate: false },
-)
 
 router.beforeEach((to, from, next) => {
   const title = to.meta.title as string | undefined
--- src/views/home/PlaybackPage.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { usePageStatusStore } from '@/stores/pageStatusStores'
-</script>
-
-<template>
-  <div class="myPlaybackPage">
-    <button @click="usePageStatusStore().isPlayBackExpand = false">关闭</button>
-  </div>
-</template>
