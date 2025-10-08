9adf2ba0da80aa67d0fdac094aa621b9f71ffc48
xiaohai-ouyang <xouyang525@gmail.com>
2025-10-09 06:30:47 +0800

    添加基本页面切换功能

---
+++ src/views/playback/PlaybackPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="playback-page">
+    <button @click="back">back</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import router from '@/router'
+import { usePageStatusStore } from '@/stores/pageStatusStores'
+import { usePlaylistStore } from '@/stores/playlistStore'
+
+const playlistStore = usePlaylistStore()
+
+function back() {
+  usePageStatusStore().isPlayBackExpand = false
+  router.back()
+}
+</script>
+
+<style scoped>
+.playback-page {
+  padding: 20px;
+}
+</style>
