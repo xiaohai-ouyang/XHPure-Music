9a16aaa69138db932d37b5b042414d8b122d362a
xiaohai-ouyang <xouyang525@gmail.com>
2025-10-10 17:24:41 +0800

    把格式化歌词做成模块

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref, watch } from 'vue'
 import { useThemeStore } from './stores/themeStore'
-import { useGlobalShortcutKey } from './composable/globalShortcutKey'
+import { useGlobalShortcutKey } from './composables/globalShortcutKey'
 import { usePlaylistStore } from './stores/playlistStore'
 
 const playlistStore = usePlaylistStore()
