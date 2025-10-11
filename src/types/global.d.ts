a4333664e6ca10de1f719bd2ea54d46d14fbc11f
xiaohai-ouyang <xouyang525@gmail.com>
2025-10-11 09:44:03 +0800

    fix：修改报错

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,3 @@
 interface WindowWithDirectoryPicker extends Window {
   showDirectoryPicker?: () => Promise<FileSystemDirectoryHandle>
 }
-
-interface FileSystemDirectoryHandle {
-  values(): AsyncIterableIterator<FileSystemHandle>
-  [Symbol.asyncIterator](): AsyncIterableIterator<[string, FileSystemHandle]>
-}
