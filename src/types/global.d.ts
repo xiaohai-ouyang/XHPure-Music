f03432e4806800203103d03d0efe11a4b0938e39
xiaohai-ouyang <xouyang525@gmail.com>
2025-10-11 09:26:44 +0800

    add
    强制屏蔽报错

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,11 @@
 interface WindowWithDirectoryPicker extends Window {
   showDirectoryPicker?: () => Promise<FileSystemDirectoryHandle>
 }
+
+declare interface FileSystemDirectoryHandle {
+  /**
+   * 支持 for await...of 遍历目录条目
+   * @example for await (const [name, handle] of dirHandle) { ... }
+   */
+  [Symbol.asyncIterator]: () => AsyncIterableIterator<[string, FileSystemHandle]>
+}
