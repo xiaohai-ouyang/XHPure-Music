075674ebcfd036dc490336d77f99e7774776f69b
xiaohai-ouyang <xouyang525@gmail.com>
2025-10-11 09:37:39 +0800

    add
    fix：添加类型补丁

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,10 +2,7 @@ interface WindowWithDirectoryPicker extends Window {
   showDirectoryPicker?: () => Promise<FileSystemDirectoryHandle>
 }
 
-declare interface FileSystemDirectoryHandle {
-  /**
-   * 支持 for await...of 遍历目录条目
-   * @example for await (const [name, handle] of dirHandle) { ... }
-   */
-  [Symbol.asyncIterator]: () => AsyncIterableIterator<[string, FileSystemHandle]>
+interface FileSystemDirectoryHandle {
+  values(): AsyncIterableIterator<FileSystemHandle>
+  [Symbol.asyncIterator](): AsyncIterableIterator<[string, FileSystemHandle]>
 }
