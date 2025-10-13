73a2655b90ea056532f046fc86ffba22901dc0f3
XiaoHi <xouyang525@gmail.com>
2025-10-13 10:37:10 +0800

    fix:优化播放页压缩问题
    - 优化播放页压缩问题

--- src/views/PlaybackPage/PlaybackPage.vue
+++ src/views/PlaybackPage/PlaybackPage.vue
@@ -100,7 +100,8 @@ watch(
   .row-flex(@align:center);
   position: relative;
   height: 100vh;
-  width: 100vw;
+  min-width: 840px;
+  min-height: 670px;
   overflow: hidden;
   background-size: 300% 300%;
   animation: gradientMove 10s ease infinite;
