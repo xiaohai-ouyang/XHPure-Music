6a06d2e4d67acb48f0a1510e610e6287ed4176fd
XiaoHi <xouyang525@gmail.com>
2025-10-14 21:41:00 +0800

    refactor:优化代码
    - 优化代码

---
+++ src/components/Music/EmptyMusicState.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="empty">
+    <button @click="onPickMusic" class="add-to-list-btn">
+      <span>添加音乐</span>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Emits {
+  (e: 'pickMusic'): void
+}
+
+const emit = defineEmits<Emits>()
+
+const onPickMusic = () => {
+  emit('pickMusic')
+}
+</script>
+
+<style lang="less" scoped>
+.empty {
+  .col-flex(@align:center, @justify: center);
+  height: 100%;
+  width: 100%;
+
+  .add-to-list-btn {
+    padding: 10px;
+    color: white;
+    background-color: @lightMode-dominant-textColor;
+    font-weight: 700;
+    font-size: 25px;
+    border-radius: 10px;
+  }
+}
+</style>
---
+++ src/components/Music/MusicFunctionBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="function-bar">
+    <button class="addAll-btn" @click="onPushAllToPlaylist">全部播放</button>
+    <div class="music-num">
+      <span class="dot"></span>音乐库中有<span class="num" v-html="musicCount"></span>首歌
+    </div>
+    <div class="music-time"><span class="num" v-html="totalMinutes"></span>分钟</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  musicCount: number
+  totalMinutes: string
+}
+
+interface Emits {
+  (e: 'pushAllToPlaylist'): void
+}
+
+defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+const onPushAllToPlaylist = () => {
+  emit('pushAllToPlaylist')
+}
+</script>
+
+<style lang="less" scoped>
+.function-bar {
+  gap: 10px;
+  padding: 5px;
+  background-color: @lightMode-secondary-bgColor;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+
+  .addAll-btn {
+    background-color: @lightMode-dominant-textColor;
+    color: white;
+    padding: 10px;
+    border-radius: 8px;
+  }
+
+  .dot {
+    margin-right: 3px;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #0088ff;
+  }
+
+  &,
+  .music-num,
+  .music-time {
+    .row-flex(@align: center);
+  }
+}
+</style>
